fae8e87664b726cbd92c60f75c891976651f5384
张庭梁 <zhangtl16@mails.tsinghua.edu.cn>
2019-12-15 11:50:29 +0800

    ex19

---
+++ ex19.py
@@ -0,0 +1,10 @@
+def cheese_and_crackers(cheese, crackers):
+    print(f"{cheese} chesses {crackers} crackers!")
+
+cheese_and_crackers(1, 2)
+
+a=10
+b=20
+cheese_and_crackers(a, b)
+
+cheese_and_crackers(a+b, a+1000)
