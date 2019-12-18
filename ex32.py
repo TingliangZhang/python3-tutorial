0884987cf7bfd67c2f06d2168f484179919fadfb
张庭梁 <zhangtl16@mails.tsinghua.edu.cn>
2019-12-18 21:29:27 +0800

    ex32

--- ex32.py
+++ ex32.py
@@ -1,2 +1,27 @@
+#list V
+#tuple?
+
 the_count = [1,2,3,4,5]
 fruits = ['apple', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+for number in the_count:
+    print(f"This is count {number}.")
+
+for fruit in fruits:
+    print(fruit)
+
+for x in change:
+    print(x)
+    # Using {} caose don't know this is number or string
+    print(f"This is {x}")
+
+elements = []
+
+for i in range(1, 6): # don't include 6, the last word!
+    print(f"Adding {i} to the list.")
+    elements.append(i)
+
+for i in elements:
+    print(f"Elements was: {i}")
+
