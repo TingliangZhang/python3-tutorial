9633182c468e34f77fb13b28d90f12aa06d9289a
Tingliang <zhangtl16@mails.tsinghua.edu.cn>
2020-07-03 15:48:23 +0800

    Create ex41.py

---
+++ ex41.py
@@ -0,0 +1,19 @@
+class ztl(object):
+    print("Tina")
+
+class X0(ztl):
+    print("I like it")
+
+class X(object):
+    def __init__(self,J):
+        print(J)
+    def M(self,K):
+        print(self)
+
+J=1
+foo = X(J)
+foo.M(J)
+
+
+
+
